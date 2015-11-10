551581999c3cdb67831e2ab2490a88e9a4e1a298
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-10 17:17:16 -0500

    Refactor to make scala parsing bits reusable

--- api/app/lib/ParseBuildSbt.scala
+++ api/app/lib/ParseBuildSbt.scala
@@ -6,33 +6,9 @@ import com.bryzek.dependency.v0.models.{LanguageForm, LibraryForm}
   * Takes the contents of a build.sbt file and parses it, providing
   * access to its dependencies (libraries, languages and versions).
   */
-case class ParseBuildSbt(contents: String) {
-
-  private case class Variable(name: String, value: String)
+case class ParseBuildSbt(contents: String) extends SimpleScalaParser {
 
   private val LanguageScala = "scala"
-
-  private val lines = contents.
-    split("\n").
-    map(stripComments(_)).
-    map(_.trim).
-    filter(!_.isEmpty).
-    filter(!_.startsWith("//"))
-
-  // Pull out all lines that start w/ "val " or "var " and capture
-  // variable declarations
-  private val variables = lines.
-    filter(line => line.startsWith("val ") || line.startsWith("var ")).
-    flatMap { line =>
-      line.split("=").map(_.trim).toList match {
-        case declaration :: value :: Nil => {
-          Some(Variable(declaration.substring(declaration.indexOf(" ")).trim, stripQuotes(value)))
-        }
-        case _ => {
-          None
-        }
-      }
-    }
 
   val languages: Seq[LanguageForm] = {
     lines.
@@ -64,8 +40,6 @@ case class ParseBuildSbt(contents: String) {
   val libraries: Seq[LibraryForm] = {
     lines.
       filter(_.replaceAll("%%", "%").split("%").size >= 2).
-      map(stripComments(_)).
-      map(_.trim).
       map(_.stripSuffix(",")).
       map(_.trim).
       map { line =>
@@ -75,56 +49,5 @@ case class ParseBuildSbt(contents: String) {
         }
       }
   }.distinct.sortBy { l => (l.groupId, l.artifactId, l.version) }
-
-  def toLibraryForm(value: String): Either[String, LibraryForm] = {
-    value.replaceAll("%%", "%").split("%").map(_.trim).toList match {
-      case Nil => {
-        Left(s"Could not parse library from[$value]")
-      }
-      case groupId :: Nil => {
-        Left(s"Could not parse library from[$value] - only found groupId[$groupId]")
-      }
-      case groupId :: artifactId :: Nil => {
-        Right(
-          LibraryForm(
-            groupId = interpolate(groupId),
-            artifactId = interpolate(artifactId)
-          )
-        )
-      }
-      case groupId :: artifactId :: version :: more => {
-        Right(
-          LibraryForm(
-            groupId = interpolate(groupId),
-            artifactId = interpolate(artifactId),
-            version = Some(interpolate(version))
-          )
-        )
-      }
-    }
-  }
-
-  def interpolate(value: String): String = {
-    val formatted = stripQuotes(value)
-    variables.find(_.name == formatted) match {
-      case None => {
-        formatted
-      }
-      case Some(variable) => variable.value
-    }
-  }
-
-  def stripQuotes(value: String): String = {
-    value.stripPrefix("\"").stripSuffix("\"").trim
-  }
-
-  def stripComments(value: String): String = {
-    val i = value.indexOf("//")
-    if (i < 0) {
-      value
-    } else {
-      value.substring(0, i)
-    }
-  }
 
 }
