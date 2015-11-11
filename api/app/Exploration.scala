f51eff4cd7587452b11d23bc997a7910cab27853
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-11 09:14:59 -0500

    Simplify html parsing

--- api/app/Exploration.scala
+++ api/app/Exploration.scala
@@ -5,24 +5,19 @@ import java.net.URL
 object Exploration {
 
   def test() {
-    getHeadlinesFromUrl("http://dl.bintray.com/typesafe/maven-releases/com/typesafe/play/play_2.11/")
+    // getHeadlinesFromUrl("http://dl.bintray.com/typesafe/maven-releases/com/typesafe/play/play_2.11/")
+    getHeadlinesFromUrl("file:///web/dependency/play_2.11.html")
   }
 
-  def getHeadlinesFromUrl(url: String): Iterable[String] = {
+  def getHeadlinesFromUrl(url: String): Seq[String] = {
     val cleaner = new HtmlCleaner()
-    // val props = cleaner.getProperties
     val rootNode = cleaner.clean(new URL(url))
     rootNode.getElementsByName("a", true).flatMap { elem =>
-      println("ELEMENT: "+ elem)
-      Option(elem.getAttributeByName("class")) match {
-        case None => {
-          // ignore
-          None
-        }
-        case Some(className) => {
-          // stories might be "dirty" with text like "'", clean it up
-          Some(StringEscapeUtils.unescapeHtml4(elem.getText.toString))
-        }
+      Option(elem.getAttributeByName("href")).flatMap { rawHref =>
+        val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
+        val href =StringEscapeUtils.unescapeHtml4(rawHref)
+        println(s" - text[$text] href[$href]")
+        Some(href)
       }
     }.toSeq
   }
