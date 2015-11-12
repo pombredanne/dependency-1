a6fa045909b2cbc7f49d36339883164103da1e90
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-12 17:25:19 -0500

    Explore how to map nested objects by convention

--- api/app/generated/Parsers.scala
+++ api/app/generated/Parsers.scala
@@ -1,12 +1,17 @@
 import anorm._
-import org.joda.time.DateTime
-import java.util.UUID
 
 package io.flow.common.v0.anorm {
 
   object Reference {
-    def parser(column: String): RowParser[io.flow.common.v0.models.Reference] = {
-      SqlParser.get[UUID](column) map {
+
+    def parserByPrefix(prefix: String, separator: String = ".") = parser(
+      guid = s"${prefix}${separator}guid"
+    )
+
+    def parser(
+      guid: String
+    ): RowParser[io.flow.common.v0.models.Reference] = {
+      SqlParser.get[_root_.java.util.UUID](guid) map {
         case guid => {
           io.flow.common.v0.models.Reference(
             guid = guid
@@ -17,11 +22,24 @@ package io.flow.common.v0.anorm {
   }
 
   object Audit {
-    def parser(table: String): RowParser[io.flow.common.v0.models.Audit] = {
-      SqlParser.get[DateTime](s"$table.created_at") ~
-      Reference.parser(s"$table.created_by_guid") ~
-      SqlParser.get[DateTime](s"$table.updated_at") ~
-      Reference.parser(s"$table.updated_by_guid") map {
+
+    def parserByPrefix(prefix: String, separator: String = ".") = parser(
+      createdAt = s"${prefix}${separator}created_at",
+      createdByPrefix = s"${prefix}${separator}created_by",
+      updatedAt = s"${prefix}${separator}updated_at",
+      updatedByPrefix = s"${prefix}${separator}updated_by"
+    )
+
+    def parser(
+      createdAt: String,
+      createdByPrefix: String,
+      updatedAt: String,
+      updatedByPrefix: String
+    ): RowParser[io.flow.common.v0.models.Audit] = {
+      SqlParser.get[_root_.org.joda.time.DateTime](createdAt) ~
+      Reference.parserByPrefix(updatedByPrefix, "_") ~
+      SqlParser.get[_root_.org.joda.time.DateTime](updatedAt) ~
+      Reference.parserByPrefix(updatedByPrefix, "_") map {
         case createdAt ~ createdBy ~ updatedAt ~ updatedBy => {
           io.flow.common.v0.models.Audit(
             createdAt = createdAt,
@@ -32,6 +50,7 @@ package io.flow.common.v0.anorm {
         }
       }
     }
+
   }
 
 }
@@ -39,10 +58,21 @@ package io.flow.common.v0.anorm {
 package com.bryzek.dependency.v0.anorm {
 
   object Language {
-    def parser(table: String): RowParser[com.bryzek.dependency.v0.models.Language] = {
-      SqlParser.get[UUID](s"$table.guid") ~
-      SqlParser.get[String](s"$table.name") ~
-      io.flow.common.v0.anorm.Audit.parser(table) map {
+
+    def parserByPrefix(prefix: String, separator: String = ".") = parser(
+      guid = s"${prefix}${separator}guid",
+      name = s"${prefix}${separator}name",
+      auditPrefix = s"${prefix}${separator}audit"
+    )
+
+    def parser(
+      guid: String = "guid",
+      name: String = "name",
+      auditPrefix: String = "audit"
+    ): RowParser[com.bryzek.dependency.v0.models.Language] = {
+      SqlParser.get[_root_.java.util.UUID](guid) ~
+      SqlParser.get[String](name) ~
+      io.flow.common.v0.anorm.Audit.parserByPrefix(auditPrefix, "_") map {
         case guid ~ name ~ audit => {
           com.bryzek.dependency.v0.models.Language(
             guid = guid,
