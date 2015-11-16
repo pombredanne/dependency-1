52017bbfef5e080057158552a3e97ff3dcfeb3f3
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-16 11:51:05 -0500

    Update UI

--- www/app/lib/UiData.scala
+++ www/app/lib/UiData.scala
@@ -2,8 +2,18 @@ package lib
 
 import com.bryzek.dependency.v0.models.User
 
+sealed trait Section
+
+object Section {
+  case object Dashboard extends Section
+  case object Projects extends Section
+  case object Languages extends Section
+  case object Libraries extends Section
+}
+
 case class UiData(
   requestPath: String,
+  section: Option[Section] = None,
   title: Option[String] = None,
   headTitle: Option[String] = None,
   user: Option[User] = None,
