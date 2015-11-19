b5468d4d81725ed115998c913f18acb3f74bc8a1
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-19 17:48:11 -0500

    Upgrade to latest lib-play. Auth now working in www

--- www/app/controllers/ApplicationController.scala
+++ www/app/controllers/ApplicationController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import com.bryzek.dependency.lib.{DependencyClientProvider, UiData}
 import io.flow.play.clients.UserTokensClient
-import io.flow.play.controllers.IdentifiedRestController
+import io.flow.play.controllers.IdentifiedController
 import play.api._
 import play.api.i18n._
 import play.api.mvc._
@@ -11,12 +11,27 @@ class ApplicationController @javax.inject.Inject() (
   val messagesApi: MessagesApi,
   val userTokensClient: UserTokensClient,
   val dependencyClientProvider: DependencyClientProvider
-) extends Controller with IdentifiedRestController with I18nSupport {
+) extends Controller
+    with IdentifiedController
+    with I18nSupport
+{
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
   override def unauthorized[A](request: Request[A]): Result = {
     Redirect(routes.LoginController.index(return_url = Some(request.path))).flashing("warning" -> "Please login")
+  }
+
+  override def user(
+    session: play.api.mvc.Session,
+    headers: play.api.mvc.Headers
+  ) (
+    implicit ec: scala.concurrent.ExecutionContext
+  ): scala.concurrent.Future[Option[io.flow.user.v0.models.User]] = {
+    session.get("user_guid") match {
+      case None => scala.concurrent.Future { None }
+      case Some(userGuid) => userTokensClient.getUserByToken(userGuid)
+    }
   }
 
   def index() = Identified { implicit request =>
