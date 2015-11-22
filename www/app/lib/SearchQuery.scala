c9d236997a39bc66ed3fd7565d4c0a298048b2c8
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-22 11:53:56 -0500

    Simplify query parser

--- www/app/lib/SearchQuery.scala
+++ www/app/lib/SearchQuery.scala
@@ -1,6 +1,7 @@
 package com.bryzek.dependency.lib
 
 import scala.util.{Failure, Success, Try}
+import play.api.Logger
 
 case class SearchQuery(
   groupId: Option[String] = None,
@@ -15,7 +16,40 @@ case class SearchQuery(
       "version" -> version
     ).filter { case (key, value) => !value.map(_.trim).getOrElse("").isEmpty }.
       map { case (key, value) => s"$key=${value.get.trim}" }.
-      mkString(" and ")
+      mkString(" ")
+  }
+
+}
+
+object SearchQuery {
+
+  def parse(value: String): SearchQuery = {
+    var q = SearchQuery()
+    value.replaceAll("\\s+", " ").replaceAll("\\s*=\\s*", "=").trim match {
+      case "" => {
+        // No-op
+      }
+      case v => {
+        v.split("\\s+").foreach { piece =>
+          piece.split("=").toList match {
+            case key :: value :: Nil => {
+              key.toLowerCase match {
+                case "groupid" => q = q.copy(groupId = Some(value))
+                case "artifactid" => q = q.copy(artifactId = Some(value))
+                case "version" => q = q.copy(version = Some(value))
+                case _ => {
+                  Logger.warn(s"Invalid query[$q] component[$v] contained unrecognized key[$key]")
+                }
+              }
+            }
+            case other => {
+              Logger.warn(s"Invalid query[$q] component[$v] did not contain two parts")
+            }
+          }
+        }
+      }
+    }
+    q
   }
 
 }
--- www/test/lib/SearchQuerySpec.scala
+++ www/test/lib/SearchQuerySpec.scala
@@ -8,12 +8,14 @@ class SearchQuerySpec extends Specification {
     SearchQuery().query must beEqualTo("")
     SearchQuery(groupId = Some("me.apidoc")).query must beEqualTo("groupId=me.apidoc")
     SearchQuery(artifactId = Some("util")).query must beEqualTo("artifactId=util")
-    SearchQuery(version = Some("1.0")).query must beEqualTo("version=1.0")
+    SearchQuery(version = Some("1.0.0")).query must beEqualTo("version=1.0.0")
     SearchQuery(
       groupId = Some("me.apidoc"),
       artifactId = Some("util"),
       version = Some("1.0")
-    ).query must beEqualTo("groupId=me.apidoc and artifactId=util and version=1.0")
+    ).query must beEqualTo(
+      "groupId=me.apidoc artifactId=util version=1.0"
+    )
   }
 
   "queryString ignores empty strings" in {
@@ -22,6 +24,21 @@ class SearchQuerySpec extends Specification {
 
   "queryString trims space" in {
     SearchQuery(groupId = Some("  me.apidoc  ")).query must beEqualTo("groupId=me.apidoc")
+  }
+
+  "parse" in {
+    SearchQuery.parse("").query must beEqualTo("")
+    SearchQuery.parse("groupId=me.apidoc").query must beEqualTo("groupId=me.apidoc")
+    SearchQuery.parse("  groupId   =   me.apidoc  ").query must beEqualTo("groupId=me.apidoc")
+    SearchQuery.parse("  groupid   =   me.apidoc  ").query must beEqualTo("groupId=me.apidoc")
+
+    SearchQuery.parse("artifactId=util").query must beEqualTo("artifactId=util")
+    SearchQuery.parse("artifactid=util").query must beEqualTo("artifactId=util")
+    SearchQuery.parse("version = 1.0.0").query must beEqualTo("version=1.0.0")
+    SearchQuery.parse("version = 1.0.0-dev").query must beEqualTo("version=1.0.0-dev")
+    SearchQuery.parse("groupId=me.apidoc artifactId=util version=1.0.0").query must beEqualTo(
+      "groupId=me.apidoc artifactId=util version=1.0.0"
+    )
   }
 
 }
