58464142eedb47d07ca3611bf265d87887a510b3
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-02 16:06:24 -0500

    Iterate on recommendations schema

--- schema/scripts/20151202-153045.sql
+++ schema/scripts/20151202-153045.sql
@@ -2,12 +2,10 @@ drop table if exists project_library_recommendations;
 drop table if exists project_binary_recommendations;
 
 create table project_library_recommendations (
-  guid                         uuid primary key,
-  project_guid                 uuid not null references projects,
-  from_library_version_guid    uuid not null references library_versions,
-  to_library_version_guid      uuid not null references library_versions,
-  latest_library_version_guid  uuid not null references library_versions,
-  constraint project_library_recommendations_from_to_ck check (from_library_version_guid != to_library_version_guid)
+  guid                                 uuid primary key,
+  project_guid                         uuid not null references projects,
+  from_project_library_version_guid    uuid not null references project_library_versions,
+  to_library_version_guid              uuid not null references library_versions
 );
 
 comment on table project_library_recommendations is '
@@ -22,9 +20,8 @@ comment on table project_library_recommendations is '
 
 select schema_evolution_manager.create_basic_audit_data('public', 'project_library_recommendations');
 create index on project_library_recommendations(project_guid);
-create index on project_library_recommendations(from_library_version_guid);
+create index on project_library_recommendations(from_project_library_version_guid);
 create index on project_library_recommendations(to_library_version_guid);
-create index on project_library_recommendations(latest_library_version_guid);
 
 create unique index
  project_library_recommendations_project_guid_from_not_del_un_idx
@@ -32,12 +29,10 @@ create unique index
  where deleted_at is null;
 
 create table project_binary_recommendations (
-  guid                         uuid primary key,
-  project_guid                 uuid not null references projects,
-  from_binary_version_guid     uuid not null references binary_versions,
-  to_binary_version_guid       uuid not null references binary_versions,
-  latest_binary_version_guid   uuid not null references binary_versions,
-  constraint project_binary_recommendations_from_to_ck check (from_binary_version_guid != to_binary_version_guid)
+  guid                                 uuid primary key,
+  project_guid                         uuid not null references projects,
+  from_project_binary_version_guid     uuid not null references project_binary_versions,
+  to_binary_version_guid               uuid not null references binary_versions
 );
 
 comment on table project_binary_recommendations is '
@@ -54,7 +49,6 @@ select schema_evolution_manager.create_basic_audit_data('public', 'project_binar
 create index on project_binary_recommendations(project_guid);
 create index on project_binary_recommendations(from_binary_version_guid);
 create index on project_binary_recommendations(to_binary_version_guid);
-create index on project_binary_recommendations(latest_binary_version_guid);
 
 create unique index
  project_binary_recommendations_project_guid_from_not_del_un_idx
