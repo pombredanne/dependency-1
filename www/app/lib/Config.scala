196ba346fe340a43be8091b91e5c6bc0418f9f18
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-04 00:50:08 -0500

    Implement proper links for search results

--- www/app/lib/Config.scala
+++ www/app/lib/Config.scala
@@ -1,6 +1,6 @@
 package com.bryzek.dependency.lib
 
-import com.bryzek.dependency.v0.models.{Recommendation, RecommendationType}
+import com.bryzek.dependency.v0.models.{BinarySummary, ItemSummary, ItemSummaryUndefinedType, LibrarySummary, ProjectSummary, Recommendation, RecommendationType}
 import io.flow.play.util.DefaultConfig
 import java.net.URLEncoder
 
@@ -24,11 +24,19 @@ object Config {
   }
 
   def recommendationUrl(recommendation: Recommendation): String = {
-    
     recommendation.`type` match {
       case RecommendationType.Library => controllers.routes.LibrariesController.show(recommendation.`object`.guid).url
       case RecommendationType.Binary => controllers.routes.BinariesController.show(recommendation.`object`.guid).url
       case RecommendationType.UNDEFINED(_) => "#"
+    }
+  }
+
+  def itemSummaryUrl(summary: ItemSummary): String = {
+    summary match {
+      case BinarySummary(guid, _) => controllers.routes.BinariesController.show(guid).url
+      case LibrarySummary(guid, _, _) => controllers.routes.LibrariesController.show(guid).url
+      case ProjectSummary(guid, _) => controllers.routes.ProjectsController.show(guid).url
+      case ItemSummaryUndefinedType(_) => "#"
     }
   }
 
