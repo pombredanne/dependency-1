8a4275d8de1a863469425272abb4ad7efe1faca0
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-09 08:14:51 -0500

    Add TODO

--- api/app/actors/MainActor.scala
+++ api/app/actors/MainActor.scala
@@ -90,6 +90,7 @@ class MainActor(name: String) extends Actor with ActorLogging with Util {
     }
 
     case m @ MainActor.Messages.ProjectDeleted(guid) => withVerboseErrorHandler(m) {
+      // TODO: Cleanup recommendations for this project
       projectActors.remove(guid).map { context.stop(_) }
       searchActor ! SearchActor.Messages.SyncProject(guid)
     }
