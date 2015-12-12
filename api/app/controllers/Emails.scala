745183b32b26a40c1e5677fb1307615d094a3a56
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-12 14:04:57 -0500

    Protect hack email page with configuration (which is absent in prod)

--- api/app/controllers/Emails.scala
+++ api/app/controllers/Emails.scala
@@ -6,6 +6,7 @@ import com.bryzek.dependency.api.lib.Person
 import com.bryzek.dependency.actors._
 import io.flow.play.clients.UserTokensClient
 import io.flow.play.controllers.AnonymousController
+import io.flow.play.util.DefaultConfig
 import play.api.mvc._
 import play.api.libs.json._
 import java.util.UUID
@@ -15,6 +16,9 @@ class Emails @javax.inject.Inject() (
   val userTokensClient: UserTokensClient
 ) extends Controller with AnonymousController with Helpers {
 
+  private[this] val TestEmailAddressName = "com.bryzek.dependency.test.email"
+  private[this] lazy val TestEmailAddress = DefaultConfig.optionalString(TestEmailAddressName)
+
   override def user(
     session: play.api.mvc.Session,
     headers: play.api.mvc.Headers
@@ -23,12 +27,19 @@ class Emails @javax.inject.Inject() (
   ) = scala.concurrent.Future { None }
 
   def get() = Action { request =>
-    val user = UsersDao.findByEmail("mbryzek@alum.mit.edu").getOrElse {
-      sys.error("User not found")
+    TestEmailAddress match {
+      case None => Ok(s"Set the $TestEmailAddressName property to enable testing")
+      case Some(email) => {
+        UsersDao.findByEmail("mbryzek@alum.mit.edu") match {
+          case None => Ok(s"No user with email address[$email] found")
+          case Some(user) => {
+            Ok(
+              EmailMessage(Publication.DailySummary).generate(user.guid, Person(user.email.get,user.name))
+            )
+          }
+        }
+      }
     }
-    Ok(
-      EmailMessage(Publication.DailySummary).generate(user.guid, Person(user.email.get,user.name))
-    )
   }
 
 }
