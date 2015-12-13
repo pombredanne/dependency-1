14703e2e8f580b225703e5e1a43323c59a7872d8
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-13 10:20:09 -0500

    Update spec for changes in Urls class

--- lib/src/test/scala/UrlsSpec.scala
+++ lib/src/test/scala/UrlsSpec.scala
@@ -5,28 +5,30 @@ import org.scalatest.{FunSpec, Matchers}
 
 class UrlsSpec extends FunSpec with Matchers with Factories {
 
+  private[this] lazy val urls = Urls()
+
   it("recommendation") {
     val binary = makeRecommendation(`type` = RecommendationType.Binary)
-    Urls.recommendation(binary) should be(s"/binaries/${binary.`object`.guid}")
+    urls.recommendation(binary) should be(s"/binaries/${binary.`object`.guid}")
 
     val library = makeRecommendation(`type` = RecommendationType.Library)
-    Urls.recommendation(library) should be(s"/libraries/${library.`object`.guid}")
+    urls.recommendation(library) should be(s"/libraries/${library.`object`.guid}")
 
     val other = makeRecommendation(`type` = RecommendationType.UNDEFINED("other"))
-    Urls.recommendation(other) should be("#")
+    urls.recommendation(other) should be("#")
   }
 
   it("itemSummary") {
     val binary = makeBinarySummary()
-    Urls.itemSummary(binary) should be(s"/binaries/${binary.guid}")
+    urls.itemSummary(binary) should be(s"/binaries/${binary.guid}")
 
     val library = makeLibrarySummary()
-    Urls.itemSummary(library) should be(s"/libraries/${library.guid}")
+    urls.itemSummary(library) should be(s"/libraries/${library.guid}")
 
     val project = makeProjectSummary()
-    Urls.itemSummary(project) should be(s"/projects/${project.guid}")
+    urls.itemSummary(project) should be(s"/projects/${project.guid}")
 
-    Urls.itemSummary(ItemSummaryUndefinedType("other")) should be("#")
+    urls.itemSummary(ItemSummaryUndefinedType("other")) should be("#")
   }
 
 }
