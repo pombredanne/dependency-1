5a245aa207742b05592846a892064f698db57de8
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-19 16:49:11 -0500

    Add direct link to file

--- www/app/lib/Config.scala
+++ www/app/lib/Config.scala
@@ -1,6 +1,6 @@
 package com.bryzek.dependency.www.lib
 
-import com.bryzek.dependency.v0.models.{BinarySummary, ItemSummary, ItemSummaryUndefinedType, LibrarySummary, ProjectSummary, Recommendation, RecommendationType}
+import com.bryzek.dependency.v0.models.{BinarySummary, ItemSummary, ItemSummaryUndefinedType, LibrarySummary, ProjectSummary, Recommendation, RecommendationType, Scms}
 import io.flow.play.util.DefaultConfig
 import java.net.URLEncoder
 
@@ -23,6 +23,26 @@ object Config {
         s"$key=" + URLEncoder.encode(value, "UTF-8")
     }.mkString("&")
 
+  }
+
+  /**
+    * Returns full URL to the file with the specified path
+    */
+  def scmsUrl(scms: Scms, uri: String, path: String): String = {
+    val join = if (path.startsWith("/")) {
+      ""
+    } else {
+      "/"
+    }
+
+    scms match {
+      case Scms.Github => {
+        uri + Seq("/blob/master", path).mkString(join)
+      }
+      case Scms.UNDEFINED(_) => {
+        Seq(uri, path).mkString(join)
+      }
+    }
   }
 
 }
