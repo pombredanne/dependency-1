b21f8ee3b665b7e15090dbd79b2d520d29f85a9f
Paolo Lim <paolo@flow.io>
2016-07-15 15:40:51 -0400

    Upgrade and bump (#38)
    * Upgrade and bump
    
    * Fix token client

--- api/app/lib/DefaultTokenClient.scala
+++ api/app/lib/DefaultTokenClient.scala
@@ -4,7 +4,7 @@ import db.{TokensDao, UsersDao}
 import io.flow.common.v0.models.UserReference
 import io.flow.token.v0.interfaces.Client
 import io.flow.token.v0.errors.UnitResponse
-import io.flow.token.v0.models.{AuthenticationForm, TokenReference, Token => FlowToken}
+import io.flow.token.v0.models.{TokenAuthenticationForm, TokenReference, Token => FlowToken}
 import org.joda.time.DateTime
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -22,6 +22,20 @@ class DefaultTokenClient() extends Client {
 
 class Tokens() extends io.flow.token.v0.Tokens {
 
+  override def getVersions(
+    id: _root_.scala.Option[Seq[String]] = None,
+    tokenId: _root_.scala.Option[Seq[String]] = None,
+    limit: Long = 25,
+    offset: Long = 0,
+    sort: String = "journal_timestamp",
+    requestHeaders: Seq[(String, String)] = Nil
+  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[Seq[io.flow.token.v0.models.TokenVersion]] = throw new UnsupportedOperationException()
+
+  override def getCleartextById(
+    id: String,
+    requestHeaders: Seq[(String, String)] = Nil
+  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[io.flow.token.v0.models.Cleartext] = throw new UnsupportedOperationException()
+
   override def get(
     id: _root_.scala.Option[Seq[String]] = None,
     token: _root_.scala.Option[String] = None,
@@ -33,11 +47,11 @@ class Tokens() extends io.flow.token.v0.Tokens {
     token.flatMap { t =>
       TokensDao.findByToken(t) match {
         case Some(t) => {
-          Some(FlowToken(id = t.id, user = UserReference(t.user.id), createdAt = new DateTime))
+          Some(FlowToken(id = t.id, user = UserReference(t.user.id), createdAt = new DateTime, partial = t.id))
         }
 
         case None => {
-          UsersDao.findById(t).map { u => FlowToken(id = u.id, user = UserReference(id = u.id), createdAt = new DateTime) }
+          UsersDao.findById(t).map { u => FlowToken(id = u.id, user = UserReference(id = u.id), createdAt = new DateTime, partial = u.id) }
         }
       }
     }.toSeq
@@ -62,5 +76,5 @@ class Tokens() extends io.flow.token.v0.Tokens {
     requestHeaders: Seq[(String, String)]
   )(implicit ec: scala.concurrent.ExecutionContext) = throw new UnsupportedOperationException()
 
-  override def postAuthentications(authenticationForm: AuthenticationForm, requestHeaders: Seq[(String, String)])(implicit ec: ExecutionContext): Future[TokenReference] = ???
+  override def postAuthentications(authenticationForm: TokenAuthenticationForm, requestHeaders: Seq[(String, String)])(implicit ec: ExecutionContext): Future[TokenReference] = ???
 }
--- www/app/lib/DefaultTokenClient.scala
+++ www/app/lib/DefaultTokenClient.scala
@@ -3,7 +3,7 @@ package com.bryzek.dependency.www.lib
 import io.flow.common.v0.models.UserReference
 import io.flow.token.v0.interfaces.Client
 import io.flow.token.v0.errors.UnitResponse
-import io.flow.token.v0.models.{AuthenticationForm, TokenReference, Token => FlowToken}
+import io.flow.token.v0.models.{TokenAuthenticationForm, TokenReference, Token => FlowToken}
 import org.joda.time.DateTime
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -21,6 +21,20 @@ class DefaultTokenClient() extends Client {
 
 class Tokens() extends io.flow.token.v0.Tokens {
 
+  override def getVersions(
+    id: _root_.scala.Option[Seq[String]] = None,
+    tokenId: _root_.scala.Option[Seq[String]] = None,
+    limit: Long = 25,
+    offset: Long = 0,
+    sort: String = "journal_timestamp",
+    requestHeaders: Seq[(String, String)] = Nil
+  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[Seq[io.flow.token.v0.models.TokenVersion]] = throw new UnsupportedOperationException()
+
+  override def getCleartextById(
+    id: String,
+    requestHeaders: Seq[(String, String)] = Nil
+  )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[io.flow.token.v0.models.Cleartext] = throw new UnsupportedOperationException()
+
   override def get(
     id: _root_.scala.Option[Seq[String]] = None,
     token: _root_.scala.Option[String] = None,
@@ -30,7 +44,7 @@ class Tokens() extends io.flow.token.v0.Tokens {
     requestHeaders: Seq[(String, String)] = Nil
   )(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[Seq[io.flow.token.v0.models.Token]] = Future {
     token.map { t=>
-      FlowToken(id = t, user = UserReference(t), createdAt = new DateTime)
+      FlowToken(id = t, user = UserReference(t), createdAt = new DateTime, partial = t)
     }.toSeq
   }
 
@@ -53,5 +67,5 @@ class Tokens() extends io.flow.token.v0.Tokens {
     requestHeaders: Seq[(String, String)]
   )(implicit ec: scala.concurrent.ExecutionContext) = throw new UnsupportedOperationException()
 
-  override def postAuthentications(authenticationForm: AuthenticationForm, requestHeaders: Seq[(String, String)])(implicit ec: ExecutionContext): Future[TokenReference] = ???
+  override def postAuthentications(authenticationForm: TokenAuthenticationForm, requestHeaders: Seq[(String, String)])(implicit ec: ExecutionContext): Future[TokenReference] = ???
 }
